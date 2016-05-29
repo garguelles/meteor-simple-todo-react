b767d155499d1b7ac617f7671b2ca735c3f76dc7
Gerard Arguelles <gerard.arguelles@live.com>
2016-05-30 03:42:56 +0800

    added tasks collections

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -3,12 +3,13 @@ import React, { Component, PropTypes } from 'react';
 // Task component - represents a single todo item
 export default class Task extends Component {
   render() {
+    console.log('task', this.props.task);
     return (
       <li>{this.props.task.text}</li>
     );
   }
 }
- 
+
 Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
