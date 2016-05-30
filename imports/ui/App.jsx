7f51e006c5b5335e957a77890781c369cd163a6c
Gerard Arguelles <gerard.arguelles@live.com>
2016-05-30 15:30:52 +0800

    [feature] add, updated, delete todo item

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,21 @@ import Task from './Task.jsx';
 // App component - represents the whole app
 class App extends Component {
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // Find the text field via the React ref
+    const text = this.refs.textInput.value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    this.refs.textInput.value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => {
       return <Task key={task._id} task={task} />
@@ -17,6 +32,14 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+              />
+          </form>
         </header>
         <ul>
           {this.renderTasks()}
@@ -32,6 +55,6 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch()
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
   };
 }, App);
