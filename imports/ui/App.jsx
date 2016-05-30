d168e43e226d62386128ff8ab8b0108f1e22a9a8
Gerard Arguelles <gerard.arguelles@live.com>
2016-05-30 15:47:23 +0800

    [feature] store component state, filter, show pending tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,20 @@ import Task from './Task.jsx';
 // App component - represents the whole app
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
 
@@ -22,7 +36,11 @@ class App extends Component {
   }
 
   renderTasks() {
-    return this.props.tasks.map((task) => {
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => {
       return <Task key={task._id} task={task} />
     });
   }
@@ -31,7 +49,17 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+              />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
@@ -50,11 +78,13 @@ class App extends Component {
 }
 
 App.propTypes = {
-  tasks: PropTypes.array.isRequired
+  tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired
 };
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count()
   };
 }, App);
